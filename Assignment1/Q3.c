dd5aad9fc6760f888d5df8a7465049619c57e906
capn-crunch-debugz <79498379+capn-crunch-debugz@users.noreply.github.com>
2022-10-03 15:49:53 -0700

    update

--- Assignment1/Q3.c
+++ Assignment1/Q3.c
@@ -6,6 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 char toUpperCase(char ch)
 {
@@ -25,6 +26,13 @@ char toLowerCase(char ch)
     return ch;                          
 }
 
+char toUnderScore(char ch)
+{
+    // Convert ch to lower case, assuming it is in upper case currently
+    
+    return ch == '_';                          
+}
+
 int stringLength(char s[])
 {
     // Return the length of the string
@@ -36,78 +44,32 @@ int stringLength(char s[])
     return count;
 }
 
+void camelCase( char* word){
+    int s = 0, d = 0;
 
-void camelCase(char* word) 
-{
-    /*Convert to camelCase*/
-    int sLength = stringLength(word);
-    int i,j,first=0,change=0;
-
-    //remove staring char '_',*,numbers,$ from starting
-    for (i = 0; i < sLength; i++){
-        if (word[0] == '_' || (word[0] >= 33 && word[0] <=64)){
-            for (j = 0; j < sLength; j++){
-                word[j] = word[j + 1];
-            }
-        --sLength;
+    while( ( word[d] = word[s] ) != '\0' ) {
+        if( isalpha( word[d] ) ) {
+            word[ d ] = toLowerCase( word[ d ] );
+            d++, s++;
+            continue;
         }
+        while( word[s] && !isalpha( word[s] ) ) s++;
+        if( word[s] )
+            word[d++] = toUpperCase( word[s++] );
     }
+    word[ 0 ] = tolower( word[ 0 ] );
+}
 
-    //remove any special chars if any in the string
-    for (i = 0; i < sLength; i++){
-        if (word[i] == '_' || (word[i] >= 33 && word[i] <= 64)){
-            if (first == 0){
-                word[i] = '_';
-                first = 1;
-            }
-
-            for (j = i+first; j < sLength; j++){
-                word[j] = word[j + 1];
-
-            }
-        --sLength;
+int isValid(char *word){
+    int i;
+    for(i = 0; word[i] != '\0'; ++i)
+    {
+        if(!((word[i] >= 'a' && word[i] <= 'z') || (word[i] >= 'A' && word[i] <= 'Z')) )
+        {
+            return 0;
         }
+        else return 1;
     }
-
-    //remove trailing _ from word
-    i = sLength-1;
-
-    while (word[i] == '_'){
-        i = --sLength;
-    }
-
-    word[i+1] = '\0';
-    sLength = i + 1;
-
-    //check if all the words upper before _
-    for (i = 0; i < sLength; i++){
-        if (word[i] == '_'){
-            break;
-        }
-    if(isupper(word[i]))
-    word[i] = toLowerCase(word[i]);
-    }
-
-    for(i = 0; i < sLength; i++){
-        if (word[i] == '_'){
-            if (islower(word[i + 1])){
-                word[i] = toUpperCase(word[i + 1]);
-                change = 1;
-            }
-            else{
-                word[i + 1] = toLowerCase(word[i + 1]);
-                change = 0;
-            }
-
-    //move all the elements after ith loc to left
-        if(change == 1)
-            for (j = i+1; j < sLength; j++){
-                word[j] = word[j + 1];
-            }
-            --sLength;
-        }
-    }
-
 }
 
 int main()
@@ -118,6 +80,10 @@ int main()
 	// Read the string from the keyboard
 	printf("Enter word: ");
     scanf("%s", wordArray);
+
+    /*if(isValid(wordArray) == 0){
+        printf("Invalid input string\n");
+    }*/
 	
 	// Call camelCase
 	camelCase(wordArray);
